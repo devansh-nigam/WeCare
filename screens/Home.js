b6d36456bed0d96f8f958565d928116eb40fecdb
615219378 <devansh.nigam@bt.com>
2023-03-17 23:34:16 +0530

    Added README.md

--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,7 @@ import { HeartIcon } from 'react-native-heroicons/solid';
 const Home = () => {
   return (
     <SafeAreaView className="bg-red-400 flex-1 justify-center">
-      <Text className="font-bold text-white text-center text-xl">Home <HeartIcon size={24} fill="#fff"/></Text>
+      <Text className="font-bold text-white text-center text-xl">WeCare <HeartIcon size={24} fill="#fff"/></Text>
     </SafeAreaView>
   )
 }
